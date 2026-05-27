1f2fe8c20cd73e566b615860d6c89d6b8642110b
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-05-27 13:46:04 +0300

    Add name and years active filters to artist search

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -29,10 +29,21 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
 const buildQuery = (criteria) => {
   const query = {};
 
+  if (criteria.name) {
+    query.$text = { $search: criteria.name };
+  }
+
   if (criteria.age) {
     query.age = {
       $gte: criteria.age.min,
       $lte: criteria.age.max
+    };
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
     };
   }
 
